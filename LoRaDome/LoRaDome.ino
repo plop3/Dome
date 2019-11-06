38baf8df8c17dad1e4893a608cf803747631b6a0
Serge CLAUS <sclaus@edfbdx.fr>
2019-11-06 16:52:49 +0100

    Ajout TM1638

--- LoRaDome/LoRaDome.ino
+++ LoRaDome/LoRaDome.ino
@@ -1,33 +1,12 @@
-/**
-   The MySensors Arduino library handles the wireless radio link and protocol
-   between your home built sensors/actuators and HA controller of choice.
-   The sensors forms a self healing radio network with optional repeaters. Each
-   repeater and gateway builds a routing tables in EEPROM which keeps track of the
-   network topology allowing messages to be routed to nodes.
-
-   Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
-   Copyright (C) 2013-2019 Sensnology AB
-   Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
-
-   Documentation: http://www.mysensors.org
-   Support Forum: http://forum.mysensors.org
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   version 2 as published by the Free Software Foundation.
-
- *******************************
-
-   REVISION HISTORY
-   Version 1.0 - tekka
-   Contribution by Berk
-
-   DESCRIPTION
-   The ESP32 gateway sends data received from sensors to the WiFi link.
-   The gateway also accepts input on ethernet interface, which is then sent out to the radio network.
-
-   Make sure to fill in your ssid and WiFi password below.
+/*
+  Gestion LoRa de l'abri du telescope
+  Serge CLAUS
+  GPL V3
+  Version 1.1
+  22/10/2018-06/11/2019
 */
+
+//---------------------------------------PERIPHERIQUES-----------------------------------------------
 
 // WiFi OTA
 #include <WiFi.h>
@@ -85,15 +64,37 @@ TM1638 module(4, 17, 25); // (4, 17, 25)
 
 #include <MySensors.h>
 
+// NTP
+#include <NTPClient.h>
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Timer
+#include <SimpleTimer.h>
+SimpleTimer timer;
+
+//---------------------------------------Macros---------------------------------------------------
+
+//---------------------------------------CONSTANTES-----------------------------------------------
+
+//---------------------------------------VARIABLES GLOBALES---------------------------------------
+
 // TM1638
-#define NiveauAff 0
+byte NiveauAff=1;		// Intensité de l'affichage
+String formattedDate;	// Client NTP
+int TypeAff = 1; 		// 0: Eteint, 1: Heure GMT, 2: T° /H%
+bool StateAff = true;	// Etat de l'affichage (M/A par bouton 2)
+int compte10=0;			// Compteur pour exécuter des commandes toutes les 10s
+int compte5=0;			// Compteur pour exécuter des commandes toutes les 5s
+
+//---------------------------------------SETUP-----------------------------------------------
 
 void setup()
 {
   // Afficheur TM1638
-  //module.setupDisplay(1, 3);
-  //module.setDisplayToString("Start");
-  //module.setLED(TM1638_COLOR_RED, 0);
+  module.setupDisplay(1, 3);
+  module.setDisplayToString("Start");
+  module.setLED(TM1638_COLOR_RED, 0);
 
   // WiFi
   Serial.begin(9600);
@@ -137,12 +138,102 @@ void setup()
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+// NTP
+  timeClient.begin();
+  //timeClient.setTimeOffset(3600); On reste en GMT
 
+  // Timer
+  timer.setInterval(1000, FuncSec);
+
+  // TODO Eteint l'afficheur si les portes sont fermées
+  
   // Setup locally attached sensors
   MyMessage msgD(2, V_TRIPPED);	// Dome
   MyMessage msgP(3, V_TRIPPED);	// Portes
   MyMessage msgT(4, V_STATUS);	// Télescope parqué
+  
+  
 }
+
+//---------------------------------------BOUCLE PRINCIPALE------------------------------------
+
+void loop()
+{
+  String SerMsg;
+  String ret;
+  // Send locally attached sensors data here
+  timer.run();
+  ArduinoOTA.handle();
+  //Serial.println("C?#");
+  //delay(1000);
+  
+  // Lecture des infos provenant de l'Arduino Nano
+  if (Serial.available()) {
+    SerMsg = Serial.readStringUntil(35);
+    if (SerMsg == "PA") {
+      ret = GetScopeInfo(":hP#");
+      Serial.println(ret);
+    }
+    else if (SerMsg == "HO") {
+      GetScopeInfo(":hC#");
+      Serial.println("0");
+    }
+    else if (SerMsg == "FN") {
+      ret = GetScopeInfo(":GVN#");
+    }
+	else if (SerMsg=="DO") {
+		send(msgD.set(1));
+	}
+	else if (SerMsg=="DF") {
+		send(msgD.set(0));
+	}
+	else if (SerMsg=="PO") {
+		send(msgP.set(1));
+	}
+	else if (SerMsg=="PF") {
+		send(msgP.set(0));
+	}
+	else if (SerMsg=="TP") {
+		send(msgT.set(1));
+	}
+	else if (SerMsg=="TN") {
+		send(msgT.set(0));
+	}
+  }
+  // Lecture des boutons TM1638
+  byte keys = module.getButtons();
+  switch (keys) {
+	case 1:	//T1 Affiche l'heure
+		TypeAff=1;
+		break;
+	case 2:	// T2 T° H%
+		TypeAff=2;
+		break;
+	case 4:	// T3 T° miroir, point de rosée
+		TypeAff=3;
+		break;
+	case 8:	// T4
+		break;
+	case 16:	// T5
+		break;
+	case 32:	// T6 Augmente l'intensité de l'éclairage
+		if (NiveauAff<7) NiveauAff++;
+		module.setupDisplay(StateAff, NiveauAff);
+		delay(300);
+		break;
+	case 64:	// T7 Diminue l'intensité de l'éclairage
+		if (NiveauAff>0) NiveauAff--;
+		module.setupDisplay(StateAff, NiveauAff);
+		delay(300);
+		break;
+	case 128:	// T8	Allume/Eteint l'affichage
+		StateAff = !StateAff;
+		module.setupDisplay(StateAff, NiveauAff);
+		delay(300);
+  }
+}
+
+//---------------------------------------FONCTIONS MYSENSORS----------------------------------
 
 void presentation()
 {
@@ -181,48 +272,50 @@ void receive(const MyMessage &message) {
 	}
 }
 
-void loop()
-{
-  String SerMsg;
-  String ret;
-  // Send locally attached sensors data here
-  ArduinoOTA.handle();
-  Serial.println("C?#");
-  delay(1000);
-  if (Serial.available()) {
-    SerMsg = Serial.readStringUntil(35);
-    if (SerMsg == "PA") {
-      ret = GetScopeInfo(":hP#");
-      Serial.println(ret);
-    }
-    else if (SerMsg == "HO") {
-      GetScopeInfo(":hC#");
-      Serial.println("0");
-    }
-    else if (SerMsg == "FN") {
-      ret = GetScopeInfo(":GVN#");
-    }
-	else if (SerMsg=="DO") {
-		send(msgD.set(1));
+//---------------------------------------FONCTIONS--------------------------------------------
+
+// Fonction executée toutes les secondes
+void FuncSec() {
+	compte5++;
+	compte10++;
+	// Toutes les 5s
+	if (compte5==5) {
+		compte5=0;
+		// TODO Lecture de l'état de chauffe du miroir
 	}
-	else if (SerMsg=="DF") {
-		send(msgD.set(0));
+	// Toutes les 10s
+	if (compte10==10) {
+		// Fonctions exécutées toutes les 10s
+		compte10=0;
+		switch (TypeAff) {
+			case 2:
+				break;
+			case 3:
+				break;
+		}
+		// TODO Lecture de l'état du télescope: Park, Tracking...
 	}
-	else if (SerMsg=="PO") {
-		send(msgP.set(1));
-	}
-	else if (SerMsg=="PF") {
-		send(msgP.set(0));
-	}
-	else if (SerMsg=="TP") {
-		send(msgT.set(1));
-	}
-	else if (SerMsg=="TN") {
-		send(msgT.set(0));
-	}
+// Toutes les 1s
+  switch (TypeAff) {
+    case 0: // Pas d'affichage
+      module.setDisplayToString("        ");
+      break;
+    case 1: // Affichage de l'heure
+      if (WiFi.status() != WL_CONNECTED) {
+
+        module.setDisplayToString("--------");
+      }
+      else {
+        while (!timeClient.update()) {
+          timeClient.forceUpdate();
+        }
+        formattedDate = timeClient.getFormattedTime();
+        module.setDisplayToString(formattedDate);
+      }
+      break;
   }
 }
-
+// Récupération/envoi des infos du télescope depuis OnStepESPServer
 String GetScopeInfo(String msg) {
   String ret;
   WiFiClient client;
@@ -237,5 +330,18 @@ String GetScopeInfo(String msg) {
   }
   client.stop();
   return ret;
-
+}
+// Affiche un message sur le TM1638
+void AffTM(String ch) {
+  ch = ch + "        ";
+  ch = ch.substring(0, 8);
+  module.setDisplayToString(ch);
+}
+// Affiche 2 champs formattés sur le TM1638
+void AffTM2(String ch1, String ch2) {
+  ch1 = ch1 + "    ";
+  ch2 = ch2 + "    ";
+  ch1 = ch1.substring(0, 4);
+  ch2 = ch2.substring(0, 4);
+  module.setDisplayToString(ch1 + ch2);
 }
